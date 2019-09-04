57b0fa47d49b6c3792681289349b4f8779e683d4
Zoe Delilo <zoe.delilo@leidos.com>
2019-09-04 16:19:07 -0400

    init

---
+++ src/main/java/jacc/JaccMojo.java
@@ -0,0 +1,75 @@
+package jacc;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import java.io.*;
+import java.util.ArrayList;
+
+
+/**
+ * Goal which touches a timestamp file.
+ *
+ * @goal touch
+ * 
+ * @phase process-sources
+ */
+@Mojo (name = "generate", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
+public class JaccMojo
+    extends AbstractMojo
+{
+	@Parameter (defaultValue = "${basedir}/output/", required = false)
+	private File outputDirectory;
+	
+	@Parameter (required = true)
+	private File sourceDirectory;
+	
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    MavenProject project;
+	
+	private static final String FILE_EXTENSION= ".jacc";
+
+
+    public void execute()
+        throws MojoExecutionException
+    {
+        File o = outputDirectory;
+        File f = sourceDirectory;
+        if ( !o.exists() )
+        {
+            o.mkdirs();
+        }
+        String [] files = f.list();
+        ArrayList<String> validJacc = new ArrayList<String>();
+        if(files == null) System.out.println("Error, no files contained in source directory.");
+        else {
+	        for(String fname : files)
+	        	if(fname.contains(FILE_EXTENSION))
+	        		validJacc.add(fname);
+	        if(validJacc.isEmpty()) System.out.println("Error, no valid JACC files.");
+	        else {
+	        	jacc.CommandLine.main((String []) validJacc.toArray());
+	        }
+        }
+       
+    }
+}
